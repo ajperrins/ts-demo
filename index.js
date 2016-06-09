43661264d4ddc5a30ddc5ddf2810b8009bc64ce4
Perrins <Andrew.Perrins@turner.com>
2016-06-09 14:49:41 -0400

    Flesh out subsetting

--- index.js
+++ index.js
@@ -18,7 +18,40 @@ var Demo = (function () {
         this.category = Demo.firstCharToCategoryMap[name[0]];
     }
     Demo.prototype.toString = function () {
-        return "I am " + DemoCategory[this.category];
+        return this.name;
+    };
+    Demo.prototype.startAge = function () {
+        var matchArray = this.name.match(/(\d{1,2})[+\-]/);
+        return parseInt(matchArray[1], 10);
+    };
+    Demo.prototype.endAge = function () {
+        var matchArray = this.name.match(/-(\d{1,2})/);
+        if (matchArray)
+            return parseInt(matchArray[1], 10);
+        else
+            return Infinity;
+    };
+    /**
+     * A demo is a subset if
+     *
+     * @param other The comparison target
+     */
+    Demo.prototype.isSubsetOf = function (other) {
+        // Subset on age range
+        var result = !(other.startAge() > this.startAge()) && !(other.endAge() < this.endAge());
+        // Subset on 'category'
+        result = result && ((other.category === DemoCategory.People && (this.category === DemoCategory.Females || this.category == DemoCategory.Males)) ||
+            (this.category === DemoCategory.Males && other.category === DemoCategory.Males) ||
+            (this.category === DemoCategory.Females && other.category === DemoCategory.Females));
+        return result;
+    };
+    Demo.allDemos = function () {
+        return _.map(_.filter(demos_1.demos, function (demo) {
+            return new Demo(demo).category !== undefined;
+        }), function (validDemoName) { return new Demo(validDemoName); });
+    };
+    Demo.byName = function (name) {
+        return _.find(Demo.allDemos(), function (d) { return d.name.trim() === name; });
     };
     Demo.firstCharToCategoryMap = {
         'M': DemoCategory.Males,
@@ -28,8 +61,11 @@ var Demo = (function () {
     };
     return Demo;
 }());
-var validDemos = _.map(_.filter(demos_1.demos, function (demo) {
-    return new Demo(demo).category !== undefined;
-}), function (validDemoName) { return new Demo(validDemoName); });
-_.each(validDemos, function (d) { return console.log(d.toString()); });
+_.each(Demo.allDemos(), function (demo) {
+    console.log("===== " + demo.toString());
+    var subsets = _.filter(Demo.allDemos(), function (d) {
+        return d.isSubsetOf(demo);
+    });
+    _.each(subsets, function (d) { return console.log("....... " + d.toString()); });
+});
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -21,10 +21,51 @@ class Demo {
   category: DemoCategory;
 
   toString() {
-    return `I am ${DemoCategory[this.category]}`;
+    return this.name;
   }
 
-  static firstCharToCategoryMap: { [key: string]: DemoCategory; } = {
+  startAge(): number {
+    var matchArray = this.name.match(/(\d{1,2})[+\-]/);
+    return parseInt(matchArray[1], 10);
+  }
+
+  endAge(): number {
+    var matchArray = this.name.match(/-(\d{1,2})/);
+    if (matchArray) return parseInt(matchArray[1], 10);
+    else return Infinity;
+  }
+
+  /**
+   * A demo is a subset if 
+   * 
+   * @param other The comparison target
+   */
+  isSubsetOf(other: Demo): boolean {
+    
+    // Subset on age range
+    let result = !(other.startAge() > this.startAge()) && !(other.endAge() < this.endAge());
+    
+    // Subset on 'category'
+    result = result && (
+       (other.category === DemoCategory.People && (this.category === DemoCategory.Females || this.category == DemoCategory.Males)) ||
+       (this.category === DemoCategory.Males && other.category === DemoCategory.Males) ||
+       (this.category === DemoCategory.Females && other.category === DemoCategory.Females)
+    )
+    
+    return result;
+  }
+
+  static allDemos(): Demo[] {
+    return _.map(_.filter(demos, (demo) =>
+      new Demo(demo).category !== undefined
+    ), (validDemoName) => new Demo(validDemoName));
+  }
+  
+  static byName(name:string):Demo{
+    return _.find(Demo.allDemos(), (d) => d.name.trim() === name);
+  }
+
+  private static firstCharToCategoryMap: { [key: string]: DemoCategory; } = {
     'M': DemoCategory.Males,
     'F': DemoCategory.Females,
     'P': DemoCategory.People,
@@ -32,8 +73,13 @@ class Demo {
   };
 }
 
-const validDemos: Demo[] = _.map(_.filter(demos, (demo) =>
-  new Demo(demo).category !== undefined
-), (validDemoName) => new Demo(validDemoName));
 
-_.each(validDemos, (d) => console.log(d.toString()));
+
+
+_.each(Demo.allDemos(), (demo) =>{
+  console.log(`===== ${demo.toString()}`);
+  var subsets = _.filter(Demo.allDemos(), (d) => {
+    return d.isSubsetOf(demo);
+  })
+  _.each(subsets, d=> console.log(`....... ${d.toString()}`));
+})
